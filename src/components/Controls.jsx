095e08dbe97ae6559949c769ed532458a06e37cd
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-18 17:00:12 +0300

    Added Custom Select (part_1)

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { Search } from './Search'
+import { CustomSelect } from './CustomSelect'
 
 export const Controls = () => {
     const [search, setSearch] = useState('')
     return (
         <div>
             <Search search={search} setSearch={setSearch} />
+            <CustomSelect />
         </div>
     )
 }
