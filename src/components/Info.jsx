90e68b4198a24d8ca3035a7fa9d62d5fbc5c9b65
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-21 15:56:58 +0300

    Data output about borders country

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -58,6 +58,37 @@ const ListItem = styled.li`
   }
 `;
 
+const Meta = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  gap: 1.5rem;
+  flex-direction: column;
+  align-items: flex-start;
+
+  & > b {
+    font-weight: var(--fw-bold);
+  }
+
+  @media (min-width: 767px) {
+    flex-direction: row;
+    align-items: center;
+  }
+`;
+
+const TagGroup = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+`;
+
+const Tag = styled.span`
+  padding: 0 1rem;
+  background-color: var(--colors-ui-base);
+  box-shadow: var(--shadow);
+  line-height: 1.5;
+  cursor: pointer;
+`;
+
 export const Info = (props) => {
     const {
         name,
@@ -119,6 +150,18 @@ export const Info = (props) => {
                         </ListItem>
                     </List>
                 </ListGroup>
+                <Meta>
+                    <b>Border Countries</b>
+                    {!borders.length ? (
+                        <span>There is no border countries</span>
+                    ) : (
+                        <TagGroup>
+                            {borders.map((border) => (
+                                <Tag key={border}>{border}</Tag>
+                            ))}
+                        </TagGroup>
+                    )}
+                </Meta>
             </div>
         </Wrapper>
     )
