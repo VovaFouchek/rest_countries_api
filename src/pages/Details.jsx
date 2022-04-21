f0e5c129b43c9d0404d9de66766d762db8d36883
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-21 14:43:47 +0300

    Navigation to "Back"(Home page)

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,31 @@
-import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from "react-router-dom";
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
 
 export const Details = () => {
     const params = useParams();
     let { name } = params;
+    let navigate = useNavigate();
+
+    const [country, setCountry] = useState(null);
+
+    console.log(country);
+
+    useEffect(() => {
+        axios.get(searchByCountry(name)).then(
+            ({ data }) => setCountry(data[0])
+        );
+    }, [name]);
+
     return (
-        <div>Details {name}</div>
+        <div>
+            <Button onClick={() => navigate(`/`)}>
+                <IoArrowBack /> Back
+            </Button>
+            <div>Details {name}</div>
+        </div>
     )
 }
